d5f6d7ce11ee5cf6b791d8695236d6e15f471d42
david <davidrfuentes@pm.me>
2022-08-03 08:37:54 -0400

    add assertions

--- main.py
+++ main.py
@@ -1,36 +1,35 @@
-from art import logo
 import os
 
+from art import logo
 
 
 def find_highest_bidder(bidding_record):
     highest_bid = 0
-    winner = ''
+    winner = ""
     for bidder in bidding_record:
         bid_amount = bidding_record[bidder]
         if bid_amount > highest_bid:
             highest_bid = bid_amount
             winner = bidder
-    print(f'The winner is {winner} with a bid of ${highest_bid}')
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
 
 
-def main():
+if __name__ == '__main__':
     bidders = {}
     bidding_finished = False
 
     while not bidding_finished:
-        print(logo)
-        name = input('What is your name?: ')
-        bid_price = int(input('What is your bid?: $'))
-        bidders[name] = bid_price
-        should_continue = input('Are the any other bidders? Type \'yes\' or \'no\'.\n')
-        os.system('clear')
+        try:
+            print(logo)
+            name = input("What is your name?: ")
+            bid_price = input("What is your bid?: $")
+            assert bid_price.isnumeric(), "Please, enter a valid bid"
+            bidders[name] = bid_price
+            should_continue = input("Are the any other bidders? Type 'yes' or 'no'.\n")
+            os.system('clear')
 
-        if should_continue == 'no':
-            bidding_finished = True
-            find_highest_bidder(bidders)
-
-
-
-if __name__ == '__main__':
-    main()
+            if should_continue == 'no':
+                bidding_finished = True
+                find_highest_bidder(bidders)
+        except AssertionError as ae:
+            print(ae)
